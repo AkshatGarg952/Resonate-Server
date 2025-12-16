f9f0ce88dd245e75871c2df4a794d52c3f2e9291
akshat45 <gargakshat952@gmail.com>
2025-12-16 20:26:11 +0530

    Modified the response format of an api

--- src/controllers/diagnostics.controller.js
+++ src/controllers/diagnostics.controller.js
@@ -40,64 +40,6 @@ const RANGES = {
   ast: [10, 40],
 };
 
-// export const uploadDiagnostics = async (req, res) => {
-//   try {
-//     const { uid } = req.user;
-    
-//     if (!req.file)
-//       return res.status(400).json({ message: "PDF file required" });
-
-  
-//     const uploadResult = await cloudinary.uploader.upload_stream(
-//       {
-//         resource_type: "raw",        
-//         folder: "resonate-reports",           
-//         format: "pdf"
-//       },
-//       async (error, result) => {
-
-//         if (error) throw error;
-
-//         const pdfUrl = result.secure_url;
-
-
-//         const record = await Diagnostics.create({
-//           userId: uid,
-//           pdfUrl,
-//           status: "pending"
-//         });
-
-//         const user = await User.findOne({firebaseUid: uid})
-
-//         const parsingResponse = await axios.post(
-//           `${process.env.MICROSERVICE_URL}/parse-report`,
-//           { pdfUrl }
-//         );
-
-//         const biomarkers = parsingResponse.data;
-
-//         record.biomarkers = biomarkers;
-//         record.status = "completed";
-//         await record.save();
-
-
-//         await sendReportReady(user.phone);
-//         return res.json({
-//           message: "Report uploaded and parsed successfully",
-//           diagnostics: record
-//         });
-
-//       }
-//     );
-
-//     uploadResult.end(req.file.buffer);
-
-//   } catch (error) {
-//     console.error(error);
-//     return res.status(500).json({ error: error.message });
-//   }
-// };
-
 
 export const uploadDiagnostics = async (req, res) => {
   const { uid } = req.user;
@@ -180,7 +122,9 @@ export const getLatestDiagnostics = async (req, res) => {
   try {
     const { uid } = req.user;
 
-    const latest = await Diagnostics.findOne({ userId: uid }).sort({ createdAt: -1 });
+    const latest = await Diagnostics.findOne({ userId: uid })
+  .sort({ createdAt: -1 })
+  .select("biomarkers status pdfUrl updatedAt");
 
     return res.json(latest);
   } catch (error) {
@@ -194,7 +138,9 @@ export const getDiagnosticsHistory = async (req, res) => {
   try {
     const { uid } = req.user;
 
-    const history = await Diagnostics.find({ userId: uid }).sort({ createdAt: -1 });
+    const history = await Diagnostics.find({ userId: uid })
+    .sort({ createdAt: -1 })
+    .select("biomarkers status pdfUrl updatedAt");
     
     return res.json(history);
   } catch (error) {
